f56b47faf429a0c7dfed840f26e7084d7acb4326
Shamim Ahmed <shamimmahmed04@gmail.com>
2019-11-20 13:54:03 +0600

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,13 +1,9 @@
 <?php
-/****************************************************/
-/************** Created by : Vivek Gupta ************/
-/***************     www.vsgupta.in     *************/
-/***************     www.iotmonk.com     *************/
-/****************************************************/ 
 
-define('DB_USER', "id10583429_garbage_monitoring");     // Your database user name
-define('DB_PASSWORD', "01748810780sha");			// Your database password (mention your db password here)
-define('DB_DATABASE', "id10583429_garbage_monitoring"); // Your database name
+
+define('DB_USER', "id10583429_garbage_monitoring");     //  database user name
+define('DB_PASSWORD', "01748810780sha");			//  database password (mention your db password here)
+define('DB_DATABASE', "id10583429_garbage_monitoring"); //  database name
 define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
 
 ?>
